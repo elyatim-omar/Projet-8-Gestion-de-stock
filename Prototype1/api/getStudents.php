bd726961d1a50ee8ed3459aba92fd3624a3043e0
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-05-31 09:44:19 +0100

    prototype1

--- Prototype1/api/getStudents.php
+++ Prototype1/api/getStudents.php
@@ -1,6 +1,6 @@
 <?php
-$dbh = new PDO("mysql:host=localhost:3306;dbname=first", "root" , "solicode24");
-$sql = " SELECT * FROM monitor ";
+$dbh = new PDO("mysql:host=localhost:3306;dbname=schoolstd","root","solicode24");
+$sql = " SELECT * FROM schoolstudents ";
 $studentsQuery = $dbh->query($sql);
 $getStudents = $studentsQuery->fetchAll(PDO::FETCH_ASSOC);
 print_r(json_encode($getStudents));
--- Prototype1/studentapp.js
+++ Prototype1/studentapp.js
@@ -9,10 +9,10 @@ function getall() {
       studentsList = JSON.parse(data);
       console.log(studentsList);
       for (var i = 0; i < studentsList.length; i++) {
-        $(".studentsList").append('<li><h2>'+"ID: "+studentsList[i]["idmonitor"]+'</h2><h3>'+"size: "+studentsList[i]["size"]+'</h3></li>');
+        $(".studentsList").append('<li><h2>'+"Name: "+studentsList[i]["name"]+'</h2><h3>'+"FatherName: "+studentsList[i]["fathername"]+'</h3><h4>'+"Rollno: "+studentsList[i]["rollno"]+'</h4><h5>'+"Degree: "+studentsList[i]["degree"]+'</h5><h6>'+"Branch: "+studentsList[i]["branch"]+'</h6> <input type="hidden" value="" class="stdID"></li>');
       }
     }
   })
 }
 getall();
-})
+});
